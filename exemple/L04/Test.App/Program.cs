e75dfb060143d80fbd9c63901604e6a33c8e44c0
Daniel Iercan <daniel.iercan@upt.ro>
2020-10-15 08:57:29 +0300

    Implementare comanda si resultat create profile

--- exemple/L04/Test.App/Program.cs
+++ exemple/L04/Test.App/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Profile.Domain.CreateProfileWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Net;
 using static Profile.Domain.CreateProfileWorkflow.CreateProfileResult;
 
 namespace Test.App
@@ -7,10 +10,58 @@ namespace Test.App
     {
         static void Main(string[] args)
         {
-            ICreateProfileResult result = new ProfileValidationFailed();
+            var cmd = new CreateProfileCmd("Ion", string.Empty, "Ionescu", "ion.inonescu@company.com");
+            var result = CreateProfile(cmd);
 
-            
-            Console.WriteLine("Hello World!");
+            result.Match(
+                    ProcessProfileCreated,
+                    ProcessProfileNotCreated,
+                    ProcessInvalidProfile
+                );
+
+            Console.ReadLine();
+        }
+
+        private static ICreateProfileResult ProcessInvalidProfile(ProfileValidationFailed validationErrors)
+        {
+            Console.WriteLine("Profile validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+
+        private static ICreateProfileResult ProcessProfileNotCreated(ProfileNotCreated profileNotCreatedResult)
+        {
+            Console.WriteLine($"Profile not created: {profileNotCreatedResult.Reason}");
+            return profileNotCreatedResult;
+        }
+
+        private static ICreateProfileResult ProcessProfileCreated(ProfileCreated profile)
+        {
+            Console.WriteLine($"Profile {profile.ProfileId}");
+            return profile;
+        }
+
+        public static ICreateProfileResult CreateProfile(CreateProfileCmd createProfileCommand)
+        {
+            if (string.IsNullOrWhiteSpace(createProfileCommand.EmailAddress))
+            {
+                var errors = new List<string>() { "Invlaid email address" };
+                return new ProfileValidationFailed(errors);
+            }
+
+            if(new Random().Next(10) > 1)
+            {
+                return new ProfileNotCreated("Email could not be verified");
+            }
+
+            var profileId = Guid.NewGuid();
+            var result = new ProfileCreated(profileId, createProfileCommand.EmailAddress);
+
+            //execute logic
+            return result;
         }
     }
 }
